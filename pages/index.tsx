f56df0193b69601cb4bbc24bbfc0df1e9db8ebf2
Woo KyungJun <natcho9010@gmail.com>
2023-04-19 18:50:50 +0900

    fix: fix with lint

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { Inter } from 'next/font/google';
+import Image from 'next/image';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
@@ -120,5 +120,5 @@ export default function Home() {
         </a>
       </div>
     </main>
-  )
+  );
 }
