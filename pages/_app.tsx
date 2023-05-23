00b2930fb998bec2a1153baf3933e16416a4661b
hyunwoo <dusdjeks@naver.com>
2023-05-23 17:17:56 +0900

    feat: add font pretendard

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,27 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import localFont from 'next/font/local';
+
+const pretendard = localFont({
+  src: [
+    {
+      path: '../styles/font/Pretendard-Regular.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../styles/font/Pretendard-Medium.woff2',
+      weight: '500',
+      style: 'normal',
+    },
+  ],
+  variable: '--font-pretendard',
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={`${pretendard.variable} font-sans`}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
