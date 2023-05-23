546659841ea66b36450695f84cd2182d2869d49d
hyunwoo <dusdjeks@naver.com>
2023-05-24 00:16:40 +0900

    feat: add svgs with svgr

---
+++ public/svgs/index.ts
@@ -0,0 +1,17 @@
+import IconAdd from './IconAdd.svg';
+import IconCancel from './IconCancel.svg';
+import IconChevronDown from './IconChevronDown.svg';
+import IconChevronLeft from './IconChevronLeft.svg';
+import IconChevronRight from './IconChevronRight.svg';
+import IconChevronUp from './IconChevronUp.svg';
+import IconOverflow from './IconOverflow.svg';
+
+export {
+  IconAdd,
+  IconCancel,
+  IconChevronDown,
+  IconChevronLeft,
+  IconChevronRight,
+  IconChevronUp,
+  IconOverflow,
+};
