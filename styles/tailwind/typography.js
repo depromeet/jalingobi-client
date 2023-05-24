dafd494830d031472615d099659512f9d752d342
hyunwoo <dusdjeks@naver.com>
2023-05-24 22:58:10 +0900

    fix: fix style camel to kebab

--- styles/tailwind/typography.js
+++ styles/tailwind/typography.js
@@ -1,52 +1,52 @@
 module.exports = {
   // Title
-  '.font_title_medium_lg': {
+  '.font-title-medium-lg': {
     fontWeight: 500,
     fontSize: '28px',
     lineHeight: '42px',
   },
-  '.font_title_medium_md': {
+  '.font-title-medium-md': {
     fontWeight: 500,
     fontSize: '20px',
     lineHeight: '30px',
   },
-  '.font_title_medium_sm': {
+  '.font-title-medium-sm': {
     fontWeight: 500,
     fontSize: '18px',
     lineHeight: '28px',
   },
 
   // Body
-  '.font_body_regular_lg': {
+  '.font-body-regular-lg': {
     fontWeight: 400,
     fontSize: '16px',
     lineHeight: '24px',
   },
-  '.font_body_regular_sm': {
+  '.font-body-regular-sm': {
     fontWeight: 400,
     fontSize: '14px',
     lineHeight: '24px',
   },
 
   // Button
-  '.font_button_medium_lg': {
+  '.font-button-medium-lg': {
     fontWeight: 500,
     fontSize: '20px',
     lineHeight: '30px',
   },
-  '.font_button_medium_sm': {
+  '.font-button-medium-sm': {
     fontWeight: 500,
     fontSize: '16px',
     lineHeight: '24px',
   },
 
   // Caption
-  '.font_caption_medium_lg': {
+  '.font-caption-medium-lg': {
     fontWeight: 500,
     fontSize: '16px',
     lineHeight: '24px',
   },
-  '.font_caption_medium_sm': {
+  '.font-caption-medium-sm': {
     fontWeight: 500,
     fontSize: '12px',
     lineHeight: '18px',
