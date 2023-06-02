7e13c4651ea6fe12c75f906735e7a94108f23949
Na-hyunwoo <dusdjeks@naver.com>
2023-06-03 01:10:42 +0900

    feat: add bottomNavigation component

--- public/svgs/index.ts
+++ public/svgs/index.ts
@@ -1,10 +1,15 @@
+import IconAddCircle from './icon-add-circle.svg';
 import IconAdd from './icon-add.svg';
 import IconCancel from './icon-cancel.svg';
+import IconChat from './icon-chat.svg';
 import IconChevronDown from './icon-chevron-down.svg';
 import IconChevronLeft from './icon-chevron-left.svg';
 import IconChevronRight from './icon-chevron-right.svg';
 import IconChevronUp from './icon-chevron-up.svg';
+import IconNote from './icon-note.svg';
 import IconOverflow from './icon-overflow.svg';
+import IconSearch from './icon-search.svg';
+import IconUser from './icon-user.svg';
 
 export {
   IconAdd,
@@ -14,4 +19,9 @@ export {
   IconChevronRight,
   IconChevronUp,
   IconOverflow,
+  IconAddCircle,
+  IconChat,
+  IconNote,
+  IconSearch,
+  IconUser,
 };
