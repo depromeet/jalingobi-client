241c9dce9cf9d383d23e8084491777f8112d03c7
WooWan <woohobi@gmail.com>
2023-06-12 13:21:37 +0900

    design: global layout padding-x 추가

--- src/shared/components/layout/GlobalLayout.tsx
+++ src/shared/components/layout/GlobalLayout.tsx
@@ -9,7 +9,7 @@ interface GlobalLayoutProps {
 export default function GlobalLayout({ children }: GlobalLayoutProps) {
   return (
     <ApplyingFont>
-      <div className="mx-auto my-0 h-screen w-full max-w-[600px]">
+      <div className="mx-auto my-0 h-screen w-full max-w-[600px] px-5">
         {children}
       </div>
     </ApplyingFont>
