b17541d9e649f54049c27fe3717165c1bc58974f
Na-hyunwoo <dusdjeks@naver.com>
2023-06-17 21:30:39 +0900

    remove: remove eslintcache

--- src/shared/components/layout/BottomNavLayout.tsx
+++ src/shared/components/layout/BottomNavLayout.tsx
@@ -6,7 +6,6 @@ interface BottomNavLayoutProps {
   children: ReactNode;
 }
 
-// TODO: 여기 overflow-y-auto를 없애고 자식에서 맥이는게 맞는거 같아서 삭제했는데요, 사이드 이펙트 발생하면 말씀해주세요 !
 export default function BottomNavLayout({ children }: BottomNavLayoutProps) {
   return (
     <div className="relative h-screen">
