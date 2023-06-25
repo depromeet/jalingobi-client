cf20d65f7108dd2526becacb3e0784f7118473bd
Jin Seon <79953344+seonjl@users.noreply.github.com>
2023-06-26 00:20:48 +0900

    fix: env, interface naming

--- src/lib/interfaces/index.ts
+++ src/lib/interfaces/index.ts
@@ -2,7 +2,10 @@ import { operations } from './api.interface';
 
 export type UpdateChallenge = operations['updateChallenge'];
 export type AuthKakao = operations['authKakao'];
+export type AuthRefresh = operations['refreshToken'];
 
 export type AuthKakaoBody =
   AuthKakao['requestBody']['content']['application/json'];
 export type AuthKakaoResponse = AuthKakao['responses']['200']['content']['*/*'];
+export type AuthRefreshResponse =
+  AuthRefresh['responses']['200']['content']['*/*'];
--- src/service/index.ts
+++ src/service/index.ts
@@ -1,6 +1,43 @@
-import axios from 'axios';
+import axios, { AxiosInstance, CreateAxiosDefaults } from 'axios';
 
-export const httpClient = axios.create({
-  // baseURL: process.env.NEXT_PUBLIC_API_URL,
-  baseURL: 'https://jalingobi.com',
+import { authRefresh } from './auth-refresh';
+
+export const interceptAfterResponseFail =
+  (axiosInstance: AxiosInstance) => async (error: any) => {
+    const { config } = error;
+    const originalRequest = config;
+
+    if (error.response.status === 401) {
+      try {
+        const {
+          result: { accessToken },
+        } = (await authRefresh(axiosInstance)) as any;
+
+        axiosInstance.defaults.headers.common.Authorization = `${accessToken}`;
+        return Promise.resolve(axiosInstance(originalRequest));
+      } catch (err) {
+        return Promise.reject(err);
+      }
+    }
+
+    return Promise.reject(error);
+  };
+
+const setInterceptors = (axiosInstance: AxiosInstance) => {
+  axiosInstance.interceptors.response.use(
+    (response) => response,
+    interceptAfterResponseFail(axiosInstance).bind(this),
+  );
+};
+
+const createAxios = (axiosConfig: CreateAxiosDefaults) => {
+  const axiosInstance = axios.create(axiosConfig);
+
+  setInterceptors(axiosInstance);
+
+  return axiosInstance;
+};
+
+export const httpClient = createAxios({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
