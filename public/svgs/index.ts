98c5e5ec24d6aca66edd2098f5581cc193856fa7
WooWan <woohobi@gmail.com>
2023-06-28 00:44:32 +0900

    chore: add svg icons
    chore: add icon

--- public/svgs/index.ts
+++ public/svgs/index.ts
@@ -1,9 +1,11 @@
+import IconFilter2 from './filter.svg';
 import IconAddCircle from './icon-add-circle.svg';
 import IconAdd from './icon-add.svg';
 import IconArrowLeft from './icon-arrow-left.svg';
 import IconArrowRight from './icon-arrow-right.svg';
 import IconArrowUpFill from './icon-arrow-up-fill.svg';
 import IconCancel from './icon-cancel.svg';
+import IconCar from './icon-car.svg';
 import IconChat from './icon-chat.svg';
 import IconChevronDown from './icon-chevron-down.svg';
 import IconChevronLeft from './icon-chevron-left.svg';
@@ -11,10 +13,13 @@ import IconChevronRight from './icon-chevron-right.svg';
 import IconChevronUp from './icon-chevron-up.svg';
 import IconCircle from './icon-circle.svg';
 import IconClock from './icon-clock.svg';
+import IconClothes from './icon-clothes.svg';
+import IconFilter from './icon-filter.svg';
 import IconNote from './icon-note.svg';
 import IconOverflow from './icon-overflow.svg';
 import IconPolygonDown from './icon-polygon-down.svg';
 import IconPolygonUp from './icon-polygon-up.svg';
+import IconRice from './icon-rice.svg';
 import IconSearch from './icon-search.svg';
 import IconSettings from './icon-settings.svg';
 import IconTile from './icon-tile.svg';
@@ -33,6 +38,7 @@ export {
   IconChat,
   IconNote,
   IconSearch,
+  IconFilter2,
   IconUser,
   IconSettings,
   IconPolygonDown,
@@ -46,4 +52,8 @@ export {
   IconArrowRight,
   IconArrowLeft,
   IconClock,
+  IconRice,
+  IconClothes,
+  IconCar,
+  IconFilter,
 };
