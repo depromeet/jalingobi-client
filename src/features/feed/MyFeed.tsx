871ad08141b9fb03c2f3f7fd5b58196a7f6f99d7
hyunwoo <Na-hyunwoo@users.noreply.github.com>
2023-06-30 01:09:15 +0900

    refactor: emoji logic

--- src/features/feed/MyFeed.tsx
+++ src/features/feed/MyFeed.tsx
@@ -5,7 +5,7 @@ import dayjs from 'dayjs';
 
 import { IconChevronRight } from '@/public/svgs';
 import { Spacing } from '@/shared/components';
-import { emojiType, EmojiInfoType } from '@/shared/types/feed';
+import { EmojiInfoType, EmojiType } from '@/shared/types/feed';
 import { convertNumberToCurrency } from '@/shared/utils/currency';
 import { getKoreanDate } from '@/shared/utils/date';
 import { createEmojiInfo } from '@/shared/utils/emoji';
@@ -27,7 +27,7 @@ type MyFeedProps = {
 };
 
 type TEmoji = {
-  type: emojiType;
+  type: EmojiType;
   count: number;
   selected: boolean;
 };
@@ -66,7 +66,7 @@ const MyFeed = ({
 
   // TODO: 이모지 컨테이너 분리하기
   // TODO: 서버 호출 로직까지 작성한 이후에 리펙토링
-  const handleClickEmoji = (clickedEmojiType: emojiType) => {
+  const handleClickEmoji = (clickedEmojiType: EmojiType) => {
     if (clickedEmojiType === 'comment') {
       return;
     }
@@ -79,13 +79,14 @@ const MyFeed = ({
     const isClickedEmojiSelectedBefore = clickedEmoji?.selected;
 
     if (isClickedEmojiSelectedBefore) {
+      deleteEmoji.mutate({
+        recordId,
+        type: clickedEmojiType,
+      });
+
       setEmojis((prev) =>
         prev.map((emoji) => {
           if (emoji.type === clickedEmojiType) {
-            deleteEmoji.mutate({
-              recordId,
-              type: clickedEmojiType,
-            });
             return {
               ...emoji,
               selected: false,
@@ -98,14 +99,15 @@ const MyFeed = ({
       return;
     }
 
+    // TODO: debounce 적용이 필요할 수도.
+    updateEmoji.mutate({
+      recordId,
+      type: clickedEmojiType,
+    });
+
     setEmojis((prev) =>
       prev.map((emoji) => {
         if (emoji.type === clickedEmojiType) {
-          // TODO: debounce 적용이 필요할 수도.
-          updateEmoji.mutate({
-            recordId,
-            type: clickedEmojiType,
-          });
           return {
             ...emoji,
             selected: true,
