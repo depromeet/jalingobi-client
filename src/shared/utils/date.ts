4f6b7ad16e7c9f6a27c741ac258136419e03d529
WooWan <woohobi@gmail.com>
2023-07-01 01:40:24 +0900

    feat: 날짜, 시간 유틸 함수

--- src/shared/utils/date.ts
+++ src/shared/utils/date.ts
@@ -19,3 +19,25 @@ export const isFeedDateDifferent = ({
 
   return currentDate !== nextDate; // return true if the dates are different
 };
+
+export const calculateDaysBetween = (
+  startAt?: string,
+  endAt?: string,
+): number => {
+  if (!startAt || !endAt) return 0;
+  const startDate = new Date(startAt);
+  const endDate = new Date(endAt);
+  const oneDay = 24 * 60 * 60 * 1000;
+  const diffDays = Math.round(
+    Math.abs((startDate.getTime() - endDate.getTime()) / oneDay),
+  );
+  return diffDays;
+};
+
+export const getDayOfWeek = (dateStr?: string): string => {
+  if (!dateStr) return '';
+  const daysOfWeek = ['일', '월', '화', '수', '목', '금', '토'];
+  const date = new Date(dateStr);
+  const dayOfWeek = daysOfWeek[date.getDay()];
+  return dayOfWeek;
+};
