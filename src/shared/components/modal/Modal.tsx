944af3f9e37e8aedc31804072e1310d8b52a0463
WooWan <woohobi@gmail.com>
2023-07-01 01:44:19 +0900

    design: modal 세로 정렬

--- src/shared/components/modal/Modal.tsx
+++ src/shared/components/modal/Modal.tsx
@@ -18,7 +18,7 @@ const Portal = ({
   ...props
 }: DialogPrimitive.DialogPortalProps) => (
   <DialogPrimitive.Portal className={cn(className)} {...props}>
-    <div className="fixed inset-0 z-50 flex items-start justify-center sm:items-center">
+    <div className="fixed inset-0 z-50 flex items-center justify-center sm:items-center">
       {children}
     </div>
   </DialogPrimitive.Portal>
