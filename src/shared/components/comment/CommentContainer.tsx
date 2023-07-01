5c58e40c423efeaeea291841e0f1068b56f2255a
hyunwoo <Na-hyunwoo@users.noreply.github.com>
2023-07-01 20:09:13 +0900

    add: empty component in comment container

--- src/shared/components/comment/CommentContainer.tsx
+++ src/shared/components/comment/CommentContainer.tsx
@@ -9,6 +9,15 @@ export const CommentContainer = ({
   comments: CommentInfoType[];
   onClickDelete: (commentId: number) => void;
 }) => {
+  if (comments.length === 0) {
+    return (
+      <div className="font-body-regular-sm flex flex-col items-center justify-center py-20 text-gray-50">
+        <p>이 소비내역, 거지가 아닌 것 같다면 ?</p>
+        <p>코멘트로 회초리를 들어줍시다.</p>
+      </div>
+    );
+  }
+
   return (
     <div className="px-5">
       {comments.map((commentInfo) => {
