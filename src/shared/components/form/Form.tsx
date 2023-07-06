25bd6a9cf7059df5c078e303b3cd886c845e8053
WooWan <woohobi@gmail.com>
2023-07-06 23:11:19 +0900

    design: 에러 메세지 색상 변경

--- src/shared/components/form/Form.tsx
+++ src/shared/components/form/Form.tsx
@@ -159,7 +159,7 @@ const FormMessage = React.forwardRef<
     <p
       ref={ref}
       id={formMessageId}
-      className={cn('text-destructive text-sm font-medium', className)}
+      className={cn('text-sm font-medium text-system-danger', className)}
       {...props}
     >
       {body}
