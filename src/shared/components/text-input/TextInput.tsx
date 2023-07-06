ba2da092dc48a9f1244c374d6dd0c9840a6b2350
WooWan <woohobi@gmail.com>
2023-07-06 23:09:30 +0900

    design: 피그마 색상 반영

--- src/shared/components/text-input/TextInput.tsx
+++ src/shared/components/text-input/TextInput.tsx
@@ -51,7 +51,7 @@ const TextInput = React.forwardRef<HTMLInputElement, TextInputProps>(
     return (
       <div
         className={cn(
-          'font-body-regular-sm relative flex h-11 w-[265px] items-center rounded-lg border-transparent bg-gray-5 caret-[#FF916F] focus:outline-none',
+          'font-body-regular-sm relative flex h-11 w-[265px] items-center rounded-lg border-transparent bg-gray-10 caret-[#FF916F] focus:outline-none',
           className,
         )}
       >
