575187816aa4d8a91d32d37bf9aab40cd765f083
WooWan <woohobi@gmail.com>
2023-07-15 00:25:19 +0900

    design: fix mobile viewpoint

--- src/shared/components/layout/BottomNavLayout.tsx
+++ src/shared/components/layout/BottomNavLayout.tsx
@@ -2,13 +2,15 @@ import { ReactNode } from 'react';
 
 import { BottomNavigation } from '../navigation';
 
+import styles from './BottomNavLayout.module.css';
+
 type BottomNavLayoutProps = {
   children: ReactNode;
 };
 
 export default function BottomNavLayout({ children }: BottomNavLayoutProps) {
   return (
-    <div className="flex h-screen flex-col">
+    <div className={styles.layout}>
       <div className="flex-1 overflow-auto">{children}</div>
       <BottomNavigation />
     </div>
