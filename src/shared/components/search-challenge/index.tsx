c22c6232cc5e06b779eeda3aa85ece92ca319ddb
WooWan <woohobi@gmail.com>
2023-07-16 15:34:55 +0900

    feat: 이미 참여한 챌린지 클릭시 내 거지방으로 이동
    feat: 이미 참여한 챌린지 클릭시 내 거지방으로 이동

--- src/shared/components/search-challenge/index.tsx
+++ src/shared/components/search-challenge/index.tsx
@@ -1,7 +1,11 @@
-import Link from 'next/link';
+import { useRouter } from 'next/router';
 
-import { useChallengeSearch } from '@/features/challenge/queries';
+import {
+  useChallengeSearch,
+  useUserChallengeList,
+} from '@/features/challenge/queries';
 import { IconClock } from '@/public/svgs';
+import { useToast } from '@/shared/hooks/useToast';
 import { CategoryKey, SortedType } from '@/shared/types/challenge';
 import { calculateDaysLeft } from '@/shared/utils/time';
 
@@ -19,59 +23,87 @@ export default function SearchChallengeList({
   sortedType,
   filter,
 }: Props) {
+  const router = useRouter();
+  const { data: userChallengeList } = useUserChallengeList();
   const { data } = useChallengeSearch({
     category: categoryKey,
     filter,
     sortedType,
   });
+  const { setToastMessage } = useToast();
   const result = data?.result;
+
+  const checkIsParticipatedChallenge = (challengeId: number) => {
+    const isParticipated =
+      userChallengeList?.result.participatedChallenges.some(
+        (userChallenge) => userChallenge.challengeId === challengeId,
+      );
+
+    if (isParticipated) {
+      setToastMessage('이미 참여한 거지방입니다.');
+    }
+    return isParticipated;
+  };
+
   if (!result?.challenges || result.challenges.length === 0) {
     return <ChallengeNotFound />;
   }
 
+  const handleRouting = (challengeId: number) => {
+    const route = checkIsParticipatedChallenge(challengeId)
+      ? '/my-poor-room'
+      : `/search/${challengeId}`;
+
+    router.push(route);
+  };
+
   return (
     <ul className="flex flex-col gap-y-2.5">
-      {result.challenges?.map((challenge) => (
-        <Link key={challenge.id} href={`/search/${challenge.id}`}>
-          <li
+      {result.challenges?.map((challenge) => {
+        return (
+          <button
             key={challenge.id}
-            className="relative flex min-h-[130px] w-full items-start rounded-md bg-gray-5 px-5 py-4"
+            className="text-left"
+            onClick={() => handleRouting(challenge.id)}
+            type="button"
           >
-            <ImageLoader
-              src={challenge.imgUrl}
-              width={54}
-              height={54}
-              alt="item"
-              className="mr-4 aspect-square rounded-md"
-            />
-            <div className="col-span-3 flex flex-col gap-y-[1px]">
-              <div className="font-caption-medium-md flex items-center  gap-x-1 font-semibold">
-                <span className="text-gray-50">
-                  {challenge.currentPeopleCount}/
-                  {challenge.availablePeopleCount}명
-                </span>
-                <span className="text-red-400">{challenge.status}</span>
+            <li className="relative flex min-h-[130px] w-full items-start rounded-md bg-gray-5 px-5 py-4">
+              <ImageLoader
+                src={challenge.imgUrl}
+                width={54}
+                height={54}
+                alt="item"
+                className="mr-4 aspect-square rounded-md"
+              />
+              <div className="col-span-3 flex flex-col gap-y-[1px]">
+                <div className="font-caption-medium-md flex items-center  gap-x-1 font-semibold">
+                  <span className="text-gray-50">
+                    {challenge.currentPeopleCount}/
+                    {challenge.availablePeopleCount}명
+                  </span>
+                  <span className="text-red-400">{challenge.status}</span>
+                </div>
+                <h3 className="font-title-medium-sm">{challenge.title}</h3>
+                <ul className="flex gap-x-1">
+                  {challenge.keywords.map((keyword, index) => (
+                    <li
+                      key={index}
+                      className="font-caption-medium-md font-medium text-gray-50"
+                    >
+                      {keyword}
+                    </li>
+                  ))}
+                </ul>
+                <div className="font-caption-medium-md flex items-center gap-x-[2px] text-gray-60">
+                  <IconClock />
+                  <p>D{calculateDaysLeft(challenge.startAt)} /</p>
+                  {challenge.period}일 동안
+                </div>
               </div>
-              <h3 className="font-title-medium-sm">{challenge.title}</h3>
-              <ul className="flex gap-x-1">
-                {challenge.keywords.map((keyword, index) => (
-                  <li
-                    key={index}
-                    className="font-caption-medium-md font-medium text-gray-50"
-                  >
-                    {keyword}
-                  </li>
-                ))}
-              </ul>
-              <div className="font-caption-medium-md flex items-center gap-x-[2px] text-gray-60">
-                <IconClock />
-                <p>D{calculateDaysLeft(challenge.startAt)} /</p>
-                {challenge.period}일 동안
-              </div>
-            </div>
-          </li>
-        </Link>
-      ))}
+            </li>
+          </button>
+        );
+      })}
     </ul>
   );
 }
