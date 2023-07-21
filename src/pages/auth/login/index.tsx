d38bc425d2a4e24d5389c56e18073d53c055423a
WooWan <woohobi@gmail.com>
2023-07-22 02:11:38 +0900

    design: update layout

--- src/pages/auth/login/index.tsx
+++ src/pages/auth/login/index.tsx
@@ -7,7 +7,7 @@ import KakaoLogin from '@/shared/components/kakao-login';
 export default function Login() {
   return (
     <div className="relative flex h-screen flex-col items-center pb-2 pt-12">
-      <div className="absolute top-[6%] flex flex-col gap-y-4">
+      <div className="absolute top-[7.5%] flex flex-col gap-y-4">
         <IconJalingobi />
         <h1 className="font-title-medium-md font-medium text-white">
           거지들의 이야기로 쌓이는 소비습관
@@ -20,7 +20,7 @@ export default function Login() {
         src="/images/login.webp"
         alt="avatar"
       />
-      <article className="absolute bottom-[10%] flex flex-col items-center">
+      <article className="absolute bottom-[5%] flex flex-col items-center">
         <span className="font-body-regular-sm items-center rounded-full bg-gray-70 px-5 py-2.5 text-white">
           5초안에 시작하고 소비 습관 챙기기🚀
         </span>
